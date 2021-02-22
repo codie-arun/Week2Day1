c0ea4e529e9fdf963c1c920ef2419d9b5070f397
codie-arun <42159657+codie-arun@users.noreply.github.com>
2021-02-22 12:23:02 +0530

    Add files via upload
    Question 58

---
+++ Base.java
@@ -0,0 +1,7 @@
+package base;
+
+public class Base{
+	protected void amethod(){
+		System.out.println("amethod");
+	}//End of amethod
+}//End of class base
---
+++ Class1.java
@@ -0,0 +1,10 @@
+package base;
+
+public class Class1 extends Base{
+	public static void main(String argv[]){
+		Base b = new Base();
+		b.amethod();
+	}//End of main
+}//End of Class1
+
+// Compilation followed by the output "amethod"
